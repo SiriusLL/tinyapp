229b4da1f53e27fabbac318476ce88b3b2435a54
SiriusLL <sirius.love.light@gmail.com>
2021-03-14 21:09:49 +0000

    added urls_new, body parser require, randome string generater, post url to urlDatabase, reditect

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const { request } = require('express');
 const express = require('express');
 const app = express();
+//body parser to parse body that is in post
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
@@ -9,6 +12,10 @@ const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xk': 'http://www.google.com'
 };
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(2, 8)
+}
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -26,6 +33,19 @@ app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+//add new URL, must be above /urls/:shortURL because its higher in file system
+app.post('/urls', (req, res) => {
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  //console.log(shortURL);
+  urlDatabase[shortURL] = longURL
+  res.redirect(`/urls/${shortURL}`);
+  console.log(urlDatabase);
+})
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
