3955695c6c45daee3c4f8eaa4bb768ea29dd6a97
SiriusLL <sirius.love.light@gmail.com>
2021-03-15 05:32:09 +0000

    installed nodemon and intered start up stript into package.json

--- express_server.js
+++ express_server.js
@@ -15,7 +15,7 @@ const urlDatabase = {
 };
 
 const generateRandomString = () => {
-  return Math.random().toString(36).substring(2, 8)
+return Math.random().toString(36).substring(2, 8)
 }
 
 app.get('/', (req, res) => {
